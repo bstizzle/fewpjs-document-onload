0ee003159f6419cdc76089f73b03e8b2bf7ffe45
bstizzle <whereyouben@gmail.com>
2020-12-09 17:45:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateText()
+});
+
+function updateText() {
+    let text = document.getElementById("text");
+    text.textContent = 'This is really cool!';
+}
